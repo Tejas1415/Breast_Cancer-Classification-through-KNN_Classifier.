bebdd333de774fb84c26531ed6702b0a295dd9c5
Tejas Krishna Reddy <tejas.krishnareddy1415@gmail.com>
2018-09-19 02:13:21 +0530

    Update Brestcancer_classification.py

--- Brestcancer_classification.py
+++ Brestcancer_classification.py
@@ -1,10 +1,17 @@
-import numpy as np
+"""
+@author-Tejas_K_Reddy
+10-05-2018 3:55 
+
+Breast Cancer classification through KNN classifier
+"""
+
+import numpy as np            # Import necessery modules
 import pandas as pd
 from sklearn import preprocessing, cross_validation, neighbors, svm
 
 df= pd.read_csv('breast-cancer-wisconsin.data.txt')
-df.replace('?', -99999, inplace=True)  # create an outlier for missing values such that many algorithms detect that and penalise it
-df.drop(['id'],1, inplace=True)
+df.replace('?', -99999, inplace=True)  # create an outlier for missing values such that many algorithms detects that and penalises it
+df.drop(['id'],1, inplace=True) # Remove Unnecessery data from the input CSV file before modelling it.
 
 X= np.array(df.drop(['class'], 1))
 y= np.array(df['class'])
@@ -22,7 +29,7 @@ print(accuracy)                                 # around 97% accuracy found
 clf1= svm.SVC()
 clf1.fit(X_train, y_train)
 accuracy = clf1.score(X_test,y_test)
-print(accuracy)
+print(accuracy)                                 # Comparitively lesser accuracy found.
 
 # Now predict for a random example
 example = np.array([[4,1,2,1,2,2,3,2,2], [4,1,2,2,2,1,3,2,2], [8,10,10,8,6,10,9,7,2]])    # Always declare this as lists of lists
